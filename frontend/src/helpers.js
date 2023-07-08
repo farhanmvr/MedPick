094352545cd8fc1d138b81f928a67b18632b4dce
Mohammed Farhan <farhanmvr@gmail.com>
2023-07-08 13:50:47 +0530

    bug fix

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -1,4 +1,4 @@
 export const getBaseURL = () => {
-  if (process.env === 'development') return 'http://localhost:5000';
+  if (process.env.NODE_ENV === 'development') return 'http://localhost:5000';
   return 'https://medpick-backend.onrender.com';
 };
